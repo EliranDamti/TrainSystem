de239e7298d4b0c08588bcf0f3bbf6d8ace88818
EliranDamti <73951012+EliranDamti@users.noreply.github.com>
2023-05-07 19:42:40 +0300

    start work

--- TrainSystem/obj/Debug/netcoreapp3.1/View/SearchView.g.i.cs
+++ TrainSystem/obj/Debug/netcoreapp3.1/View/SearchView.g.i.cs
@@ -61,7 +61,7 @@ namespace TrainSystem.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TrainSystem;component/view/searchview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TrainSystem;V1.0.0.0;component/view/searchview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\SearchView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
